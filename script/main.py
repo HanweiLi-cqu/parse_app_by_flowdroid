4097f9f50f892cbe486803167c557049ff0d6835
lhw <1157180903@qq.com>
2023-12-10 13:01:55 +0800

    only unparsed

--- script/main.py
+++ script/main.py
@@ -1,7 +1,22 @@
 from app_parse import muti_app_parse
 from util.file_io import get_file_list
+
+def check_apk_parsed(apk_lst:list,out_dir):
+    """
+    if apk not parsed, return it
+    """
+    out_lst = get_file_list(out_dir)
+    out_parsed_apk_lst = [x.split("/")[-1].replace(".txt","") for x in out_lst]
+    res = []
+    for apk in apk_lst:
+        apk_name = apk.split("/")[-1].replace(".apk","")
+        if apk_name not in out_parsed_apk_lst:
+            res.append(apk)
+    return res
+
 if __name__=="__main__":
     apk_dir = ""
     apk_lst = get_file_list(apk_dir,endwith=".apk")
     out_dir = ""
+    apk_lst = check_apk_parsed(apk_lst,out_dir)
     muti_app_parse(apk_lst, out_dir,n_process=10)
