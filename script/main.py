24ca836102cc599d408d30a3213e197388efc1e3
lhw <1157180903@qq.com>
2023-12-10 12:40:58 +0800

    check apk_lst suffix

--- script/main.py
+++ script/main.py
@@ -2,6 +2,6 @@ from app_parse import muti_app_parse
 from util.file_io import get_file_list
 if __name__=="__main__":
     apk_dir = ""
-    apk_lst = get_file_list(apk_dir)
+    apk_lst = get_file_list(apk_dir,endwith=".apk")
     out_dir = ""
     muti_app_parse(apk_lst, out_dir,n_process=10)
