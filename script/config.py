74bb6fbfc5747979384b59128cc72b87d5622183
LHW <1157180903@qq.com>
2024-04-09 22:19:12 +0800

    change config.py

--- script/config.py
+++ script/config.py
@@ -1,8 +1,8 @@
-ANDROID_PLATFORM_PATH = "/home/lhw/android/android-sdk/platforms"
-APK_DIR = ""
-OUTPUT_DIR = ""
+ANDROID_PLATFORM_PATH = "" # platform path
+APK_DIR = "" # target apk dir path
+OUTPUT_DIR = "" # output path
 REDIS_HOST = ""
 REDIS_PORT = ""
-REDIS_KEY = ""
-FLOWDROID_JAR_PATH = "/home/lhw/flowdroid_demo/demo.jar"
+REDIS_KEY = "apkPaths" # don't change this
+FLOWDROID_JAR_PATH = "" # flowdroid path
 
